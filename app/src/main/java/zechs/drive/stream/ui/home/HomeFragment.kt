0a52148c93ad7665de8d8b9522ac7cd09591624f
zechs <vijay.vky2002@gmail.com>
2022-06-13 21:16:26 +0530

    Added viewBinding in `HomeFragment`

--- app/src/main/java/zechs/drive/stream/ui/home/HomeFragment.kt
+++ app/src/main/java/zechs/drive/stream/ui/home/HomeFragment.kt
@@ -1,6 +1,35 @@
 package zechs.drive.stream.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import zechs.drive.stream.R
+import zechs.drive.stream.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(
+            inflater, container, /* attachToParent */false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentHomeBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
