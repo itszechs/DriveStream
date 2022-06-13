01fda85b568254d6898a90303a1efb450f813f92
zechs <vijay.vky2002@gmail.com>
2022-06-13 20:42:58 +0530

    Request sign-in

--- app/src/main/java/zechs/drive/stream/ui/main/MainActivity.kt
+++ app/src/main/java/zechs/drive/stream/ui/main/MainActivity.kt
@@ -1,11 +1,18 @@
 package zechs.drive.stream.ui.main
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.Scopes
+import com.google.android.gms.common.api.Scope
 import dagger.hilt.android.AndroidEntryPoint
 import zechs.drive.stream.R
 import zechs.drive.stream.databinding.ActivityMainBinding
@@ -13,9 +20,12 @@ import zechs.drive.stream.databinding.ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +42,8 @@ class MainActivity : AppCompatActivity() {
             R.id.mainNavHostFragment
         ) as NavHostFragment
         navController = navHostFragment.navController
+
+        requestSignIn()
     }
 
     private fun doSplashScreen() {
@@ -46,6 +58,28 @@ class MainActivity : AppCompatActivity() {
                     .start()
             }
         }
+    }
+
+    private fun requestSignIn() {
+        Log.d(TAG, "Requesting sign-in")
+
+        val signInOptions = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(Scope(Scopes.DRIVE_FULL))
+            .requestEmail()
+            .build()
+
+        val client = GoogleSignIn.getClient(this, signInOptions)
+
+        val startForResult = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val intent = result.data!!
+                Log.d(TAG, "intent=$intent")
+            }
+        }
+        startForResult.launch(client.signInIntent)
     }
 
     override fun onSupportNavigateUp(): Boolean {
