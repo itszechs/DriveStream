359154a64767e55324d5fb5847eb7e22e79037f7
zechs <vijay.vky2002@gmail.com>
2022-06-14 18:42:37 +0530

    Setup `FilesViewHolder`

--- app/src/main/java/zechs/drive/stream/ui/files/adapter/FilesViewHolder.kt
+++ app/src/main/java/zechs/drive/stream/ui/files/adapter/FilesViewHolder.kt
@@ -1,13 +1,45 @@
 package zechs.drive.stream.ui.files.adapter
 
+import androidx.core.view.isGone
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.request.RequestOptions
+import zechs.drive.stream.R
 import zechs.drive.stream.data.model.DriveFile
 import zechs.drive.stream.databinding.ItemDriveFileBinding
+import zechs.drive.stream.utils.GlideApp
 
 class FilesViewHolder(
     private val itemBinding: ItemDriveFileBinding,
     val filesAdapter: FilesAdapter
 ) : RecyclerView.ViewHolder(itemBinding.root) {
 
-    fun bind(item: DriveFile) {}
+    fun bind(item: DriveFile) {
+        itemBinding.apply {
+
+            val iconLink = item.iconLink128 ?: R.drawable.ic_my_drive_24
+
+            GlideApp.with(ivFileType)
+                .load(iconLink)
+                .apply(RequestOptions().override(48, 48))
+                .into(ivFileType)
+
+            tvFileName.text = item.name
+
+            val tvFileSizeTAG = "tvFileSize"
+
+            tvFileSize.apply {
+                tag = if (item.size == null) {
+                    tvFileSizeTAG
+                } else null
+
+                isGone = tag == tvFileSizeTAG
+                text = item.humanSize
+            }
+
+            root.setOnClickListener {
+                filesAdapter.onClickListener.invoke(item)
+            }
+
+        }
+    }
 }
