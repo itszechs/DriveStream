da5fe9c2bb013f9b040f6bfe7f0f24be86aea636
zechs <vijay.vky2002@gmail.com>
2022-06-14 18:33:16 +0530

    Added ViewBinding in `FilesFragment`

--- app/src/main/java/zechs/drive/stream/ui/files/FilesFragment.kt
+++ app/src/main/java/zechs/drive/stream/ui/files/FilesFragment.kt
@@ -1,6 +1,38 @@
 package zechs.drive.stream.ui.files
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import zechs.drive.stream.R
+import zechs.drive.stream.databinding.FragmentFilesBinding
 
-class FilesFragment : Fragment(R.layout.fragment_files)
+
+class FilesFragment : Fragment() {
+
+    private var _binding: FragmentFilesBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFilesBinding.inflate(
+            inflater, container, /* attachToParent */false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentFilesBinding.bind(view)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
