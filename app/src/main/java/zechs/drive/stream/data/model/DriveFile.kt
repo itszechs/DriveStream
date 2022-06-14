14b9a3c7a1aed2365586295861884aabd939f0ed
zechs <vijay.vky2002@gmail.com>
2022-06-14 18:37:27 +0530

    Added additional fields in `Drive` data model

--- app/src/main/java/zechs/drive/stream/data/model/DriveFile.kt
+++ app/src/main/java/zechs/drive/stream/data/model/DriveFile.kt
@@ -1,6 +1,7 @@
 package zechs.drive.stream.data.model
 
 import androidx.annotation.Keep
+import zechs.drive.stream.utils.util.Converter
 
 @Keep
 data class DriveFile(
@@ -9,4 +10,15 @@ data class DriveFile(
     val size: Long?,
     val mimeType: String,
     val iconLink: String?
-)
+) {
+    val humanSize = size?.let { Converter.toHumanSize(it) }
+
+    val isVideoFile = mimeType.startsWith("video/")
+
+    val isFolder = mimeType == "application/vnd.google-apps.folder"
+            || mimeType == "drive#teamDrive"
+
+    val isShortcut = mimeType == "application/vnd.google-apps.shortcut"
+
+    val iconLink128 = iconLink?.replace("16", "128")
+}
