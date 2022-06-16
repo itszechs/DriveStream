f7cc7c58b161ecb5dc072d5260cc84c17fc4df36
zechs <vijay.vky2002@gmail.com>
2022-06-16 14:52:03 +0530

    Updated `DriveHelper` added public method to retrieve access token
    - Local caching in datastore

--- app/src/main/java/zechs/drive/stream/data/remote/DriveHelper.kt
+++ app/src/main/java/zechs/drive/stream/data/remote/DriveHelper.kt
@@ -3,6 +3,7 @@ package zechs.drive.stream.data.remote
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.google.android.gms.auth.GoogleAuthUtil
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.Scope
 import com.google.api.client.extensions.android.http.AndroidHttp
@@ -11,15 +12,17 @@ import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.*
 import zechs.drive.stream.R
+import zechs.drive.stream.utils.SessionManager
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.coroutines.resume
 
 @Singleton
 class DriveHelper @Inject constructor(
-    @ApplicationContext private val context: Context
+    @ApplicationContext private val context: Context,
+    private val sessionManager: SessionManager
 ) {
 
     companion object {
@@ -32,6 +35,10 @@ class DriveHelper @Inject constructor(
     var hasSignedIn = false
         private set
 
+    /**
+     * Public method to send intent result from
+     * Google Sign-in flow
+     */
     suspend fun handleSignIn(result: Intent) {
         val drive = handleSignInResult(result)
         setDrive(drive)
@@ -46,7 +53,7 @@ class DriveHelper @Inject constructor(
 
                 val credential = GoogleAccountCredential.usingOAuth2(
                     /* context */ context,
-                    /* scopes */ listOf(DriveScopes.DRIVE_FILE)
+                    /* scopes */ listOf(DriveScopes.DRIVE)
                 ).apply { selectedAccount = googleAccount.account }
 
                 val permission = hasPermission()
@@ -58,11 +65,18 @@ class DriveHelper @Inject constructor(
                         /* HttpTransport */ AndroidHttp.newCompatibleTransport(),
                         /* jsonFactory */  GsonFactory(),
                         /* httpRequestInitializer */ credential
-                    ).setApplicationName(context.getString(R.string.app_name)).build()
+                    ).setApplicationName(
+                        context.getString(R.string.app_name)
+                    ).build()
 
                     hasSignedIn = true
                     Log.d(TAG, "hasSignedIn=$hasSignedIn")
+
                     cont.resume(driveBuilder)
+
+                    if (driveBuilder != null) {
+                        Log.d(TAG, "Drive service built successfully")
+                    }
                 } else {
                     cont.resume(null)
                     Log.d(TAG, "Required permissions not granted")
@@ -85,5 +99,52 @@ class DriveHelper @Inject constructor(
     private fun setDrive(drive: Drive?) {
         this.drive = drive
     }
+
+    /**
+     * Singular point to get access token
+     *
+     */
+    fun getAccessToken(refresh: Boolean = false): String? {
+        var accessToken: String?
+        val token = CoroutineScope(Dispatchers.IO).async {
+            val fetchAccessToken = sessionManager.fetchAccessToken()
+            if (fetchAccessToken != null && !refresh) {
+                Log.d(TAG, "Access token acquired")
+                return@async fetchAccessToken
+            } else {
+                Log.d(TAG, "Attempting to refresh access token")
+                val requestAccessToken = requestAccessToken()
+                if (requestAccessToken != null) {
+                    Log.d(TAG, "Access token refreshed successfully")
+                    sessionManager.saveAccessToken(requestAccessToken)
+                    return@async requestAccessToken
+                }
+                Log.d(TAG, "Unable to refresh access token")
+            }
+            Log.d(TAG, "Access token not found")
+            return@async null
+        }
+        runBlocking {
+            accessToken = token.await()
+        }
+        return accessToken
+    }
+
+    private suspend fun requestAccessToken(): String? =
+        suspendCancellableCoroutine { cont ->
+            try {
+                GoogleSignIn.getLastSignedInAccount(context)?.let {
+                    it.account?.let { account ->
+                        GoogleAuthUtil.getToken(
+                            context, account,
+                            /* scope */ "oauth2:${DriveScopes.DRIVE}"
+                        ).also { t -> cont.resume(t) }
+                    } ?: cont.resume(null)
+                } ?: cont.resume(null)
+            } catch (e: Exception) {
+                cont.resume(null)
+                Log.d(TAG, "requestAccessToken: ${e.message.toString()}")
+            }
+        }
 
 }
--- app/src/main/java/zechs/drive/stream/di/AppModule.kt
+++ app/src/main/java/zechs/drive/stream/di/AppModule.kt
@@ -7,6 +7,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import zechs.drive.stream.data.remote.DriveHelper
+import zechs.drive.stream.utils.SessionManager
 import javax.inject.Singleton
 
 
@@ -17,7 +18,15 @@ object AppModule {
     @Singleton
     @Provides
     fun provideDriveHelper(
+        @ApplicationContext appContext: Context,
+        sessionManager: SessionManager
+    ): DriveHelper = DriveHelper(appContext, sessionManager)
+
+
+    @Singleton
+    @Provides
+    fun provideSessionDataStore(
         @ApplicationContext appContext: Context
-    ): DriveHelper = DriveHelper(appContext)
+    ): SessionManager = SessionManager(appContext)
 
 }
