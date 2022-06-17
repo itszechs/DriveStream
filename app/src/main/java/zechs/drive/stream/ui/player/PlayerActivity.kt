223bd4cf17c9a94fef66ded98ea124024a22c35d
zechs <vijay.vky2002@gmail.com>
2022-06-17 16:25:46 +0530

    Created `PlayerActivity`

---
+++ app/src/main/java/zechs/drive/stream/ui/player/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package zechs.drive.stream.ui.player
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import zechs.drive.stream.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
