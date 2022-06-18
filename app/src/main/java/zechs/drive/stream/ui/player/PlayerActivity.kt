d7812bdc53820bf7de5c915c71c3da8ab254dee2
zechs <vijay.vky2002@gmail.com>
2022-06-18 14:30:00 +0530

    Basic video player is up now

--- app/src/main/java/zechs/drive/stream/ui/player/PlayerActivity.kt
+++ app/src/main/java/zechs/drive/stream/ui/player/PlayerActivity.kt
@@ -1,12 +1,175 @@
 package zechs.drive.stream.ui.player
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import zechs.drive.stream.R
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.DefaultRenderersFactory.EXTENSION_RENDERER_MODE_ON
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.extractor.ts.DefaultTsPayloadReaderFactory
+import com.google.android.exoplayer2.extractor.ts.TsExtractor
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import dagger.hilt.android.AndroidEntryPoint
+import zechs.drive.stream.data.remote.DriveHelper
+import zechs.drive.stream.databinding.ActivityPlayerBinding
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class PlayerActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "PlayerActivity"
+        private const val DRIVE_API = "https://www.googleapis.com/drive/v3"
+    }
+
+    @Inject
+    lateinit var driveHelper: DriveHelper
+
+    // View binding
+    private lateinit var binding: ActivityPlayerBinding
+
+    // Exoplayer
+    private lateinit var player: ExoPlayer
+    private lateinit var dataSourceFactory: DataSource.Factory
+    private lateinit var trackSelector: DefaultTrackSelector
+
+    @Suppress("DEPRECATION")
+    private lateinit var playerView: PlayerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        hideSystemUI()
+
+        playerView = binding.playerView
+
+        initPlayer()
+        playMedia()
     }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        playMedia()
+    }
+
+    private fun initPlayer() {
+        val extractorsFactory = DefaultExtractorsFactory()
+            .setTsExtractorFlags(DefaultTsPayloadReaderFactory.FLAG_ENABLE_HDMV_DTS_AUDIO_STREAMS)
+            .setTsExtractorTimestampSearchBytes(1500 * TsExtractor.TS_PACKET_SIZE)
+
+        val rendererFactory = DefaultRenderersFactory(this)
+
+        // handles the duration of media to retain in the buffer prior
+        // to the current playback position
+        // (for fast backward seeking)
+        val loadControl = DefaultLoadControl.Builder()
+            // cache the last three minutes
+            .setBackBuffer(1000 * 60 * 3, true)
+            .setBufferDurationsMs(
+                DefaultLoadControl.DEFAULT_MIN_BUFFER_MS,
+                50 * 1000, // buffering goal, s -> ms
+                DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS,
+                DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS
+            )
+            .build()
+
+        trackSelector = DefaultTrackSelector(this).apply {
+            parameters = this.buildUponParameters()
+                .setPreferredAudioLanguage("en")
+                .build()
+        }
+
+        val httpDataSourceFactory = DefaultHttpDataSource
+            .Factory()
+            .setAllowCrossProtocolRedirects(true)
+
+        dataSourceFactory = DataSource.Factory {
+            httpDataSourceFactory.createDataSource()
+        }
+
+        player = ExoPlayer.Builder(this, rendererFactory)
+            .setTrackSelector(trackSelector)
+            .setMediaSourceFactory(DefaultMediaSourceFactory(dataSourceFactory, extractorsFactory))
+            .setLoadControl(loadControl)
+            .setSeekForwardIncrementMs(10_000)
+            .setSeekBackIncrementMs(10_000)
+            .build()
+    }
+
+    private fun releasePlayer() {
+        player.clearMediaItems()
+        player.release()
+        playerView.player = null
+    }
+
+    private fun playMedia() {
+        val fileId = intent.getStringExtra("fileId")
+        val title = intent.getStringExtra("title")
+
+        playerView.apply {
+            player = this@PlayerActivity.player
+            controllerHideOnTouch = true
+        }
+
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
+            .build()
+
+        if (fileId != null) {
+            val mediaItem = MediaItem.Builder()
+                .setUri(getStreamUrl(fileId))
+                .build()
+
+            player.apply {
+                setAudioAttributes(audioAttributes, true)
+                addMediaItem(mediaItem)
+                prepare()
+            }.also { it.play() }
+        }
+    }
+
+
+    private fun getStreamUrl(fileId: String): Uri {
+        val uri = Uri.parse(
+            "$DRIVE_API/files/${fileId}?supportsAllDrives=True&alt=media"
+        )
+        Log.d(TAG, "STREAM_URL=$uri")
+        return uri
+    }
+
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        player.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+
 }
