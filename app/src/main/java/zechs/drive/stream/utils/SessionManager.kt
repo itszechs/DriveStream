913d14ad0af7c8598b39f80291691a503f33a6b6
zechs <vijay.vky2002@gmail.com>
2022-06-29 14:52:08 +0530

    Updated `SessionManager`
    - Added methods to save/fetch access/refresh token

--- app/src/main/java/zechs/drive/stream/utils/SessionManager.kt
+++ app/src/main/java/zechs/drive/stream/utils/SessionManager.kt
@@ -5,33 +5,65 @@ import android.util.Log
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.first
+import zechs.drive.stream.data.model.TokenResponse
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton
 class SessionManager @Inject constructor(
-    @ApplicationContext appContext: Context
+    @ApplicationContext appContext: Context,
+    private val gson: Gson
 ) {
 
     private val sessionStore = appContext.dataStore
 
-    suspend fun saveAccessToken(accessToken: String) {
+    suspend fun saveAccessToken(data: TokenResponse) {
         val dataStoreKey = stringPreferencesKey(ACCESS_TOKEN)
+        val currentTimeInSeconds = System.currentTimeMillis() / 1000
+        val newData = data.copy(
+            expiresIn = currentTimeInSeconds + data.expiresIn
+        )
         sessionStore.edit { settings ->
-            settings[dataStoreKey] = accessToken
+            settings[dataStoreKey] = gson.toJson(newData)
         }
-        Log.d(TAG, "saveAccessToken: $accessToken")
+        Log.d(TAG, "saveAccessToken: $data")
     }
 
-    suspend fun fetchAccessToken(): String? {
+    suspend fun fetchAccessToken(): TokenResponse? {
         val dataStoreKey = stringPreferencesKey(ACCESS_TOKEN)
         val preferences = sessionStore.data.first()
         val value = preferences[dataStoreKey]
-        Log.d(TAG, "fetchAccessToken: $value")
+        val login: TokenResponse? = value?.let {
+            val type = object : TypeToken<TokenResponse?>() {}.type
+            gson.fromJson(value, type)
+        }
+        Log.d(TAG, "fetchAccessToken: $login")
+        return login
+    }
+
+    suspend fun saveRefreshToken(refreshToken: String) {
+        val dataStoreKey = stringPreferencesKey(REFRESH_TOKEN)
+        sessionStore.edit { settings ->
+            settings[dataStoreKey] = refreshToken
+        }
+        Log.d(TAG, "saveRefreshToken: $refreshToken")
+    }
+
+    suspend fun fetchRefreshToken(): String? {
+        val dataStoreKey = stringPreferencesKey(REFRESH_TOKEN)
+        val preferences = sessionStore.data.first()
+        val value = preferences[dataStoreKey]
+        Log.d(TAG, "fetchRefreshToken: $value")
         return value
+    }
+
+    suspend fun resetDataStore() {
+        sessionStore.edit { it.clear() }
     }
 
     companion object {
@@ -40,6 +72,7 @@ class SessionManager @Inject constructor(
         )
         const val TAG = "SessionManager"
         const val ACCESS_TOKEN = "ACCESS_TOKEN"
+        const val REFRESH_TOKEN = "REFRESH_TOKEN"
     }
 
 }
