08867f1b92f833cda8025c3629e0fd6e9551c83b
zechs <vijay.vky2002@gmail.com>
2022-06-30 16:40:54 +0530

    Updated Sign-out method

--- app/src/main/java/zechs/drive/stream/ui/home/HomeViewModel.kt
+++ app/src/main/java/zechs/drive/stream/ui/home/HomeViewModel.kt
@@ -2,17 +2,18 @@ package zechs.drive.stream.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.Lazy
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import zechs.drive.stream.data.remote.DriveHelper
+import zechs.drive.stream.utils.SessionManager
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val driveHelper: DriveHelper
+    private val sessionManager: Lazy<SessionManager>
 ) : ViewModel() {
 
     private val _hasLoggedOut = MutableStateFlow(false)
@@ -20,9 +21,7 @@ class HomeViewModel @Inject constructor(
 
     fun logOut() = viewModelScope.launch {
         delay(250L)
-        val client = driveHelper.getClient()
-        // client.revokeAccess()
-        client.signOut()
+        sessionManager.get().resetDataStore()
         _hasLoggedOut.value = true
     }
 
