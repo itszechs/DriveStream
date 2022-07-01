2a6fa4acff0bdf250ccf793fd009e3753ecfb6ec
zechs <vijay.vky2002@gmail.com>
2022-07-01 19:36:22 +0530

    Added `getLatestRelease()` method and data-model

---
+++ app/src/main/java/zechs/drive/stream/data/model/LatestRelease.kt
@@ -0,0 +1,18 @@
+package zechs.drive.stream.data.model
+
+import androidx.annotation.Keep
+import com.squareup.moshi.Json
+import zechs.drive.stream.BuildConfig
+
+@Keep
+data class LatestRelease(
+    val name: String,
+    @Json(name = "tag_name")
+    val tagName: String,
+    @Json(name = "html_url")
+    val htmlUrl: String
+) {
+
+    fun isLatest() = tagName != BuildConfig.VERSION_NAME
+
+}
