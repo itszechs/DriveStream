8c772de247733c5f871069c1ad8ddd51055af533
zechs <vijay.vky2002@gmail.com>
2022-07-01 19:38:47 +0530

    Update `MainViewModel` to check for latest release on startup

--- app/src/main/java/zechs/drive/stream/ui/main/MainViewModel.kt
+++ app/src/main/java/zechs/drive/stream/ui/main/MainViewModel.kt
@@ -1,5 +1,7 @@
 package zechs.drive.stream.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -7,12 +9,16 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import zechs.drive.stream.data.model.LatestRelease
+import zechs.drive.stream.data.repository.GithubRepository
 import zechs.drive.stream.utils.SessionManager
+import zechs.drive.stream.utils.state.Resource
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val sessionManager: SessionManager
+    private val sessionManager: SessionManager,
+    private val githubRepository: GithubRepository
 ) : ViewModel() {
 
     private val _isLoading = MutableStateFlow(true)
@@ -21,9 +27,14 @@ class MainViewModel @Inject constructor(
     private val _hasLoggedIn = MutableStateFlow(false)
     val hasLoggedIn = _hasLoggedIn.asStateFlow()
 
+    private val _latest = MutableLiveData<Resource<LatestRelease>>()
+    val latest: LiveData<Resource<LatestRelease>>
+        get() = _latest
+
     init {
         viewModelScope.launch {
             val status = getLoginStatus()
+            getLatestRelease() // check for update
             delay(250L)
             _isLoading.value = false
             _hasLoggedIn.value = status
@@ -35,4 +46,7 @@ class MainViewModel @Inject constructor(
         return refreshToken != null
     }
 
+    private suspend fun getLatestRelease() {
+        _latest.postValue(githubRepository.getLatestRelease())
+    }
 }
