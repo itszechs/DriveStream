2fef67840b88d2b440045073995d8a85419d8f43
zechs <vijay.vky2002@gmail.com>
2023-06-25 00:20:06 +0530

    added AdMob

--- app/src/main/java/zechs/drive/stream/ThisApp.kt
+++ app/src/main/java/zechs/drive/stream/ThisApp.kt
@@ -1,6 +1,7 @@
 package zechs.drive.stream
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
@@ -11,6 +12,7 @@ class ThisApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Firebase.crashlytics.setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
+        MobileAds.initialize(this)
     }
 
 }
