1ffdc4c589287e95959616fb39645415ad9530c7
zechs <vijay.vky2002@gmail.com>
2023-06-25 00:16:18 +0530

    added firebase
    - Analytics
    - Crashlytics

--- app/src/main/java/zechs/drive/stream/ThisApp.kt
+++ app/src/main/java/zechs/drive/stream/ThisApp.kt
@@ -1,7 +1,16 @@
 package zechs.drive.stream
 
 import android.app.Application
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ThisApp : Application()
+class ThisApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Firebase.crashlytics.setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
+    }
+
+}
